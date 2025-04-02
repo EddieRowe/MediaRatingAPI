57f6f24d8ea88ab0407f71a4b4c1781b9613ff0e
tentcoding <68378134+EddieRowe@users.noreply.github.com>
2025-04-02 09:10:32 +0100

    Add Media model, Add MediaController, add Scalar

---
+++ MediaRatingAPI/Controllers/MediaController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MediaRatingAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MediaController : ControllerBase
+    {
+        static private List<Media> medias = new List<Media>
+        {
+            new Media
+            {
+                Id = 1,
+                Title = "Bookworm",
+                ReleaseDate = DateTime.Parse("2024-08-29"),
+                Format = "Film",
+                Rating = 9
+            },
+            new Media
+            {
+                Id = 2,
+                Title = "Invincible Season 1",
+                ReleaseDate = DateTime.Parse("2021-03-25"),
+                Format = "TV",
+                Rating = 10
+            },
+            new Media
+            {
+                Id = 3,
+                Title = "Flow",
+                ReleaseDate = DateTime.Parse("2024-10-30"),
+                Format = "Film",
+                Rating = 9
+            }
+        };
+
+        [HttpGet]
+        public ActionResult<List<Media>> GetMedias()
+        {
+            return Ok(medias);
+        }
+    }
+}
---
+++ MediaRatingAPI/Media.cs
@@ -0,0 +1,11 @@
+﻿namespace MediaRatingAPI
+{
+    public class Media
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public DateTime? ReleaseDate { get; set; }
+        public string? Format { get; set; }
+        public int? Rating { get; set; }
+    }
+}
